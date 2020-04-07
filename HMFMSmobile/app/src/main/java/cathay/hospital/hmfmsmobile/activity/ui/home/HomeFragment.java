d137eef3a263a2791e52f1988fa37705c9518bce
DD0s-JCN13 <jimmylin160@gmail.com>
2020-04-07 15:02:03 +0800

    Fragment upload tmp
    Decided to build up some data catchup design on homepage, still have things to go.

--- HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ui/home/HomeFragment.java
+++ HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ui/home/HomeFragment.java
@@ -24,6 +24,8 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        final TextView divnotextView = root.findViewById(R.id.text_divNo);
+        final TextView usernametextView = root.findViewById(R.id.text_userName);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
