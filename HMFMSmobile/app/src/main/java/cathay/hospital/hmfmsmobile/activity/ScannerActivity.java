9a4712d0b31775ccc3c41ebb6f029941363afe34
DD0s-JCN13 <jimmylin160@gmail.com>
2020-04-20 23:34:12 +0800

    New activity add-in
    Add in Sanner Activity, but now has another problem, need to redesign homepage layout in order not to let the view become strange.

---
+++ HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ScannerActivity.java
@@ -0,0 +1,15 @@
+package cathay.hospital.hmfmsmobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cathay.hospital.hmfmsmobile.R;
+import android.os.Bundle;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
