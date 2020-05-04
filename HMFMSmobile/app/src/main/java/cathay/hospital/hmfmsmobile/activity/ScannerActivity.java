253b1909a4bc5f1c97bfed3fd50ee35c01cf6df7
DD0s-JCN13 <jimmylin160@gmail.com>
2020-05-04 22:12:27 +0800

    scanner activity update TMP

--- HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ScannerActivity.java
+++ HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ScannerActivity.java
@@ -1,15 +1,49 @@
 package cathay.hospital.hmfmsmobile.activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import cathay.hospital.hmfmsmobile.R;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class ScannerActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        FindView();
+        ActionBarSet();
+        setChecked();
+    }
+
+    protected void FindView(){
+        drawerLayout = findViewById(R.id.scanner_drawer);
+        toolbar = findViewById(R.id.toolbar_scanner);
+        navigationView = findViewById(R.id.nav_view_scanner);
+        bottomNavigationView = findViewById(R.id.nav_view_scanner);
+    }
+
+    protected void setChecked(){
+        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+    }
+
+    protected void ActionBarSet(){
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
