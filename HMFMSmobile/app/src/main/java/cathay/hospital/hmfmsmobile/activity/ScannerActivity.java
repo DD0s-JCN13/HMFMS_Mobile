634dfd86f19b490daeb9fabe1367bce71a30144f
DD0s-JCN13 <jimmylin160@gmail.com>
2020-05-05 19:51:19 +0800

    scanner activity update tmp
    Upload this data because changing working space

--- HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ScannerActivity.java
+++ HMFMSmobile/app/src/main/java/cathay/hospital/hmfmsmobile/activity/ScannerActivity.java
@@ -2,11 +2,16 @@ package cathay.hospital.hmfmsmobile.activity;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import cathay.hospital.hmfmsmobile.R;
+import cathay.hospital.hmfmsmobile.util.UtilTools;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -17,6 +22,7 @@ public class ScannerActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private BottomNavigationView bottomNavigationView;
     private Toolbar toolbar;
+    private boolean sysCondition = Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class ScannerActivity extends AppCompatActivity {
         FindView();
         ActionBarSet();
         setChecked();
+        NavigationDrawerSet();
+        BottomNavigationSet();
     }
 
     protected void FindView(){
@@ -45,5 +53,58 @@ public class ScannerActivity extends AppCompatActivity {
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    protected void NavigationDrawerSet(){
+        navigationView.setNavigationItemSelectedListener(item -> {
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            int id = item.getItemId();
+            switch (id){
+                case R.id.nav_home:
+                    UtilTools.goActivity(this,HomepageActivity.class);
+                    if(sysCondition){fadeSwitchAnimation();}
+                    return true;
+                case R.id.nav_checklist:
+                    UtilTools.goActivity(this,CheckListActivity.class);
+                    if(sysCondition){fadeSwitchAnimation();}
+                    return true;
+                case R.id.nav_err_loc:
+                    Toast.makeText(ScannerActivity.this, "Error Location direction",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.nav_del_loc:
+                    Toast.makeText(ScannerActivity.this, "Delete Location direction",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    protected void BottomNavigationSet(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            switch (id){
+                case R.id.action_homeapge:
+                    UtilTools.goActivity(this, HomepageActivity.class);
+                    if(sysCondition) {fadeSwitchAnimation();}
+                    return true;
+                case R.id.action_scanner:
+                    Toast.makeText(ScannerActivity.this, "Already on this page!!",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.list_item:
+                    UtilTools.goActivity(this, CheckListActivity.class);
+                    if(sysCondition) {fadeSwitchAnimation();}
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    protected void fadeSwitchAnimation(){
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
